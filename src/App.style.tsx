9512ee2e2cc49739bd4605cd1d39f58d2562c8e1
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-23 20:18:21 +0200

    Setup basic app structure

---
+++ src/App.style.tsx
@@ -0,0 +1,12 @@
+import style, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+    body {
+        background: #000;
+        color: #fff;
+    }
+`;
+
+export const HomeIntroduction = style.div`
+    text-align: center;
+`;
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { GlobalStyles, HomeIntroduction } from './App.style';
+
+const App = () => {
+  return (
+    <div>
+      <GlobalStyles />
+      <HomeIntroduction>
+        <h1>Control Center best books</h1>
+      </HomeIntroduction>
+    </div>
+  );
+}
+
+export default App;
