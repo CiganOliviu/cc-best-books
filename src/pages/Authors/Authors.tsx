88d6b381edbd1e36079c745ab6ff6a8e47594394
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-26 14:58:01 +0200

    Find item by id

--- src/pages/Authors/Authors.tsx
+++ src/pages/Authors/Authors.tsx
@@ -1,23 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { useCustomFetchAuthors } from '../../backend/apiCalls';
-import { AuthorsType } from '../../helpers/types';
+import React, { useEffect, useRef, useState } from 'react';
+import { useCustomFetchAuthors, useCustomFetchNationalities } from '../../backend/apiCalls';
+import { AuthorsType, NationalitiesType } from '../../helpers/types';
 
 const Authors = () => {
 
     const [authors, setAuthors] = useState([]);
+    const [nationalities, setNationalities] = useState([]);
+
     const { apiDataAuthors, serverErrorAuthors } = useCustomFetchAuthors();
+    const { apiDataNationalities, serverErrorNationalities } = useCustomFetchNationalities();
 
     useEffect(() => {
         if (apiDataAuthors) {
             setAuthors(apiDataAuthors);
         }
-    }, [apiDataAuthors]);
+
+        if (apiDataNationalities) {
+            setNationalities(apiDataNationalities);
+        }
+    }, [apiDataAuthors, apiDataNationalities]);
 
     useEffect(() => {
-        if (serverErrorAuthors) {
+        if (serverErrorAuthors || serverErrorNationalities) {
             throw new Error("Error fetching data from AuthorsModel")
         }
-    }, [serverErrorAuthors]);
+    }, [serverErrorAuthors, serverErrorNationalities]);
+    
+    const getNationality = (authorId: string): string => {
+
+        let result = '';
+        nationalities.forEach((nationality: NationalitiesType) => {
+            if (nationality._id === authorId[0]) {
+                result = nationality.name
+            }
+        });
+
+        return result;
+    };
 
     return (
         <div>
@@ -25,7 +44,13 @@ const Authors = () => {
             authors.map((data: AuthorsType) => {
                 return (
                     <div key={ data.last_name }>
-                        <h1>{ data.first_name } { data.last_name }</h1>
+                        <p>{ data.first_name }</p>
+                        <p>{ data.last_name }</p>
+                        <p>{ data.age }</p>
+                        <p>{ getNationality(data.nationality) } </p>
+                        <p>{ data.occupation }</p>
+                        <p>{ data.website }</p>                        
+                        <div>&nbsp;</div>
                     </div>
                 )
             })
