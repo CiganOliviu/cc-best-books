05a875ed215b36f9f78bdeb88e36b103cad6ea36
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-28 13:01:02 +0300

    Optimize importing

--- src/pages/Authors/Authors.tsx
+++ src/pages/Authors/Authors.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { useCustomFetchAuthors, useCustomFetchNationalities } from '../../backend/apiCalls';
 import { AuthorsType, NationalitiesType } from '../../helpers/types';
 import { isDataFetchedValid } from '../../helpers/validators';
