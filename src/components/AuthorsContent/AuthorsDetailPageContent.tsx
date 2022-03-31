3aedee9a526a6c89ee0ea1c5d37619b4253bef6f
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-03-31 18:16:44 +0300

    Update detail page content

--- src/components/AuthorsContent/AuthorsDetailPageContent.tsx
+++ src/components/AuthorsContent/AuthorsDetailPageContent.tsx
@@ -1,15 +1,31 @@
-import React from 'react';
-import { AssetType } from '../../helpers/types';
+import React, { useEffect, useState } from 'react';
+import { useCustomFetchNationalities } from '../../backend/apiCalls';
+import { getNationality } from '../../helpers/generalHelpers';
+import { AssetType, NationalitiesType } from '../../helpers/types';
 
 const AuthorsDetailPageContent: React.FC<AssetType> = (asset: any) => {
+    const [nationalities, setNationalities] = useState<NationalitiesType[]>([]);
+    const { apiData, serverError } = useCustomFetchNationalities();
 
-    // { getNationality(nationalities, author?.nationality) }
+    useEffect(() => {
+        if (apiData) {
+            setNationalities(apiData);
+        }
+    }, [apiData]);
+
+    useEffect(() => {
+        if (serverError) {
+            throw new Error('Unable to fetch nationalities from authors');
+        }
+    }, [serverError])
+
     return (
         <div>
             <p>{ asset?.asset?.first_name }</p>
             <p>{ asset?.asset?.last_name }</p>
             <p>{ asset?.asset?.age }</p>
             <p>{ asset?.asset?.occupation }</p>
+            <p>{ getNationality(nationalities, asset?.asset?.nationality) }</p>
             <p>{ asset?.asset?.website }</p> 
         </div>
     )
