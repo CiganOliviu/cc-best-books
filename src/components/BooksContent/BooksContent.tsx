5306119ce03b2f80041cd4224236b985f612eb85
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-01 09:15:44 +0300

    Update BooksContenet

--- src/components/BooksContent/BooksContent.tsx
+++ src/components/BooksContent/BooksContent.tsx
@@ -3,7 +3,9 @@ import { AssetType } from '../../helpers/types';
 
 const BooksContent: React.FC<AssetType> = (asset: any) => {
     return (
-        <p></p>
+        <p>
+            { asset?.asset?.title }
+        </p>
     )
 };
 
