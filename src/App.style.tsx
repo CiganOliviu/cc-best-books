f70c83973c2a8ec7ef5226f05ba8beed35924674
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-14 22:13:27 +0300

    Update part of content

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -12,12 +12,17 @@ export const GlobalStyles = createGlobalStyle`
 export const TableTitle = styled.a`
     color: #fff;
     text-decoration: none;
-    font-size: 20px;
+    font-size: 24px;
     
     :hover {
         color: #477AFF;
     }
 `;
+
+export const ContentParagraph = styled.p`
+    color: #fff;
+    font-size: 18px;
+`
 
 export const TitleContainer = styled.div`
     padding: 1%;
--- src/components/CategoriesContent/CategoriesDetailPageContent.tsx
+++ src/components/CategoriesContent/CategoriesDetailPageContent.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { AssetType } from '../../helpers/types';
+import { ContentParagraph } from '../../App.style';
 
 const CategoriesDetailPageContent: React.FC<AssetType> = (asset: any) => {
-    
+
     return (
         <div>
-            
+            <ContentParagraph>id: { asset?.asset?._id }</ContentParagraph>
+            <ContentParagraph>name: { asset?.asset?.name }</ContentParagraph>
         </div>
     )
 };
