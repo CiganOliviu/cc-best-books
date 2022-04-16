7e90395ce05393d46965592da5a55e7c75318ed2
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2022-04-16 19:40:07 +0300

    Update style-detail-page

--- src/App.style.tsx
+++ src/App.style.tsx
@@ -1,4 +1,4 @@
-import style, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import styled from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
@@ -36,10 +36,33 @@ export const InputContainer = styled.input`
     border-radius: 5%;
     background: #2C2C2C;
     color: #fff;
+    border: none;
   
     :focus {
       color: #477AFF;
       border-color: #477AFF;
+    }
+`;
+
+export const ButtonsContainer = styled.div`
+    display: flex;
+    flex-grow: 1;
+    padding: 1%;
+    justify-content: right;
+`;
+
+export const CustomButton = styled.button<{ isAlertButton: boolean }>`
+    background: ${props => props.isAlertButton ? '#e60000' : '#477AFF'};
+    color: #fff;
+    padding: 1%;
+    margin-left: 1%;
+    font-size: 20px;
+    border: none;
+    transition: all 500ms ease;
+    border-radius: 5%;
+  
+    :hover {
+      background: ${props => props.isAlertButton ? '#990000' : '#0048ff'};
     }
 `;
 
--- src/components/AuthorsContent/AuthorsDetailPageContent.tsx
+++ src/components/AuthorsContent/AuthorsDetailPageContent.tsx
@@ -3,7 +3,7 @@ import { useCustomFetchData } from '../../backend/apiCalls';
 import { getNationality } from '../../helpers/generalHelpers';
 import { AssetType, NationalitiesType } from '../../helpers/types';
 import { requestUrls } from '../../backend/requestUrls';
-import { ContentParagraph, DetailPageContainer, InputContainer } from '../../App.style';
+import { ContentParagraph, DetailPageContainer, InputContainer, CustomButton, ButtonsContainer } from '../../App.style';
 
 const AuthorsDetailPageContent: React.FC<AssetType> = (asset: any) => {
     const [nationalities, setNationalities] = useState<NationalitiesType[]>([]);
@@ -22,26 +22,33 @@ const AuthorsDetailPageContent: React.FC<AssetType> = (asset: any) => {
     }, [serverError])
     return (
         <DetailPageContainer>
-            <ContentParagraph>Profile picture:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.profile_picture } />
-            <div>&nbsp;</div>
-            <ContentParagraph>First name:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.first_name } />
-            <div>&nbsp;</div>
-            <ContentParagraph>Last name:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.last_name } />
-            <div>&nbsp;</div>
-            <ContentParagraph>Age:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.age } />
-            <div>&nbsp;</div>
-            <ContentParagraph>Occupation:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.occupation } />
-            <div>&nbsp;</div>
-            <ContentParagraph>Nationality:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ getNationality(nationalities, asset?.asset?.nationality) } />
-            <div>&nbsp;</div>
-            <ContentParagraph>Website:</ContentParagraph>
-            <InputContainer type="text" defaultValue={ asset?.asset?.website } />
+            <form>
+                <ContentParagraph>Profile picture:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.profile_picture } />
+                <div>&nbsp;</div>
+                <ContentParagraph>First name:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.first_name } />
+                <div>&nbsp;</div>
+                <ContentParagraph>Last name:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.last_name } />
+                <div>&nbsp;</div>
+                <ContentParagraph>Age:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.age } />
+                <div>&nbsp;</div>
+                <ContentParagraph>Occupation:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.occupation } />
+                <div>&nbsp;</div>
+                <ContentParagraph>Nationality:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ getNationality(nationalities, asset?.asset?.nationality) } />
+                <div>&nbsp;</div>
+                <ContentParagraph>Website:</ContentParagraph>
+                <InputContainer type="text" defaultValue={ asset?.asset?.website } />
+                <div>&nbsp;</div>
+                <ButtonsContainer>
+                    <CustomButton isAlertButton={false}>Update set of data</CustomButton>
+                    <CustomButton isAlertButton={true}>Delete set of data</CustomButton>
+                </ButtonsContainer>
+            </form>
         </DetailPageContainer>
     )
 };
